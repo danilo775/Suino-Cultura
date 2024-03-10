c4587f05bb9035e54a35f748c5917359824e1bdf
danilo775 <danilo.uba@hotmail.com>
2024-03-10 00:53:17 -0300

    adicionando estilos

--- suinocultura/src/app/components/header/header.component.ts
+++ suinocultura/src/app/components/header/header.component.ts
@@ -2,38 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { AutenticaService } from '../../services/autentica.service';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from '../../model/usuario.model'; // Certifique-se de importar o tipo Usuario
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'] // Note a correção aqui para 'styleUrls'
 })
 
-export class HeaderComponent implements OnInit{
-    private inscricao!: Subscription;
-    estaAutenticado = true;
-  
-    constructor(private autenticaService: AutenticaService, private rotas:Router, private route: ActivatedRoute) { }
-  
-  
-    ngOnInit(): void {
-      this.inscricao = this.autenticaService.usuario.subscribe(
-        usuario => {
-          this.estaAutenticado = !usuario ? false : true;
-        });
-      this.estaAutenticado = !this.autenticaService.usuario.value ? false : true;
-      console.log(this.estaAutenticado);
-    }
-  
-    ngOnDestroy(): void {
-      this.inscricao.unsubscribe();
-    }
-  
-    logout() {
-      this.autenticaService.logout();
-      console.log(this.autenticaService.logout());
-      setTimeout(() => {
-        this.rotas.navigate(['']);
-       }, 1000);
-    }
+export class HeaderComponent implements OnInit {
+  private inscricao!: Subscription;
+
+  estaAutenticado = true;
+
+  constructor(private autenticaService: AutenticaService, private rotas: Router, private route: ActivatedRoute) { }
+
+
+  ngOnInit(): void {
+    this.inscricao = this.autenticaService.usuario.subscribe(
+      (usuario: Usuario) => { // Definindo o tipo do parâmetro 'usuario' como Usuario
+        this.estaAutenticado = !usuario ? false : true;
+      });
+    this.estaAutenticado = !this.autenticaService.usuario.value ? false : true;
+    console.log(this.estaAutenticado);
+  }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
+
+  logout() {
+    this.autenticaService.logout();
+    console.log(this.autenticaService.logout());
+    setTimeout(() => {
+      this.rotas.navigate(['']);
+    }, 1000);
+  }
 }
