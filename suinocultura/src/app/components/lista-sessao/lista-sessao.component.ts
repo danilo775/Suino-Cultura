eccc4b2e314d240d25eb232f45b8939d853be8b5
danilo775 <danilo.uba@hotmail.com>
2024-04-03 23:24:20 -0300

    lista sessao ok

--- suinocultura/src/app/components/lista-sessao/lista-sessao.component.ts
+++ suinocultura/src/app/components/lista-sessao/lista-sessao.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Sessao } from '../../model/sessao.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SessaoService } from '../../services/sessao.service';
 
 @Component({
   selector: 'app-lista-sessao',
   templateUrl: './lista-sessao.component.html',
   styleUrl: './lista-sessao.component.scss'
 })
-export class ListaSessaoComponent {
+export class ListaSessaoComponent implements OnInit {
+  loadedSessao:Sessao[] = [];
+  filterTerm: string = ''; // Adicione uma variável para o termo de filtro
+  Suinos: string[] = [];
+  searchText: string = '';
+  column: string = '';
 
+  constructor(private bancoService:SessaoService, private rotas:Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.getSessao();
+    this.carregarFiltros();
+  }
+
+  getSessao() {
+    this.bancoService.getSessao().subscribe(responseData => {
+      this.loadedSessao = responseData;
+    });
+  }
+
+  // Adicione um novo método para obter suínos filtrados
+  // getFilteredSuinos() {
+  //   this.bancoService.getFilteredSessao(this.filterTerm).subscribe(responseData => {
+  //     console.log(responseData);
+  //     this.loadedSuinos = responseData;
+  //     console.log(this.loadedSuinos);
+  //   });
+  // }
+
+  editarBilhete(id:any){
+    console.log(id);
+
+  }
+
+
+  deletarSessao(id:any){
+    this.bancoService.deletarSessao(id).subscribe( () => {
+      console.log(id);
+      this.getSessao(); // Chama a função para recarregar os suínos após a exclusão
+    })
+  }
+  rediracionaPrincipal(){
+    setTimeout(() => {
+     this.rotas.navigate(['listarSuinos']);
+    }, 2000);
+
+  }
+  onSelectChange(event: any) {
+    this.column = event.target.value; // Atualiza collum com o valor da opção selecionada
+
+    console.log(event.target.value); // Aqui você pode lidar com a seleção do filtro
+  }
+
+  carregarFiltros() {
+    // Carrega os filtros disponíveis, substitua 'Suinos' com os filtros reais que você deseja usar
+    this.Suinos = ['brincoAnimal', 'dataSessao', 'atividade', 'informacao'];
+  }
 }
--- suinocultura/src/app/services/sessao.service.ts
+++ suinocultura/src/app/services/sessao.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Sessao } from '../model/sessao.model';
 import { AutenticaService } from './autentica.service';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -25,4 +26,63 @@ adicionarSessao(Sessao: {
       }
     );
     }
+    getSessao() {
+      return this.http.get<Sessao[]>(this.apiURL).pipe(
+        map((responseData: any) => {
+          const sessoesArray: Sessao[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              sessoesArray.push({ ...(responseData[key]), id: key });
+            }
+          }
+          return sessoesArray;
+        })
+      );
+    }
+
+    deletarSessao(id: string) {
+      return this.http.delete(`https://suinocultura-27005-default-rtdb.firebaseio.com/posts/${id}.json`);
+    }
+
+    editarSessao(id:string, SuinoData: {
+                                            brincoAnimal: number;
+                                            dataSessao: number;
+                                            atividade: number;
+                                            informacao: string;
+                                        }
+    ) {
+    return this.http.put(`https://suinocultura-27005-default-rtdb.firebaseio.com/posts/${id}.json`, SuinoData, {observe: 'response'});
+  }
+
+  getSuino2() {
+    return this.http.get('https://suinocultura-27005-default-rtdb.firebaseio.com/posts.json',
+    {
+      params: new HttpParams().set('print', 'pretty')
+    }
+  );
+  }
+
+  getFilteredSuino(filterTerm: string) {
+// Construa o URL para filtragem corretamente usando "equalTo"
+const filteredUrl = `${this.apiURL}?orderBy="status"&equalTo="${filterTerm}"`;
+
+// Faça a solicitação HTTP
+return this.http.get<Sessao[]>(filteredUrl);
 }
+
+getBrincosSessao() {
+return this.http.get<{ [key: string]: any }>(this.apiURL).pipe(
+  map(responseData => {
+    const brincos: string[] = [];
+    for (const key in responseData) {
+      if (responseData.hasOwnProperty(key)) {
+        brincos.push(responseData[key].brincoAnimal); // Supondo que 'brincoAnimal' seja o campo que contém o brinco do suíno
+      }
+    }
+    return brincos;
+  })
+);
+}
+
+}
+
